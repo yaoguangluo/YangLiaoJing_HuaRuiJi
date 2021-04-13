3dece48286ca969c247d2c7d4b4cfdfbd36fad6f
YaoguangLuo <2080315360@qq.com>
2021-04-13 15:02:14 +0800

    按 sonarlint 宽度 修正 码农的修养+1%
    罗瑶光

--- DetaSoundDataProcessor/OSI/OSU/OEU/node/sound/lygSlaveFilter/LygSlaveFilterInterface.java
+++ DetaSoundDataProcessor/OSI/OSU/OEU/node/sound/lygSlaveFilter/LygSlaveFilterInterface.java
@@ -6,11 +6,10 @@ import java.io.IOException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 
+import OSI.OSU.AOPM.VECS.IDUQ.GUI.flash.ThisCanvas;
 import OSI.OSU.OEQ.MCQ.GUI.OSGI.*;
-public class LygSlaveFilterInterface extends OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI
-{	
-	public LygSlaveFilterInterface() throws IOException
- 	{
+public class LygSlaveFilterInterface extends OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI{	
+	public LygSlaveFilterInterface() throws IOException{
 		thisIcon=new ImageIcon(this.getClass().getResource("med.jpg"));
 		SQ_OSU_MSQ_OSU_AVQ_ASQ_SQ_VPC_PCS=new String("lygSlave");
 		AMV_MVS_VSQ=new String("SOUND");
@@ -19,25 +18,23 @@ public class LygSlaveFilterInterface extends OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI
 		thisImage=img.getScaledInstance(30,30,java.awt.Image.SCALE_SMOOTH );
 		thisIcon = new ImageIcon(newimg);
  	}
-	public void MEI_MSU(JTextPane jTextPane) throws IOException
-	{
+	public void MEI_MSU(JTextPane jTextPane, ThisCanvas canvas) throws IOException{
 		SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ=new LygSlaveFilterView();
 		SQ_OSU_MSQ_OSU_AVQ_ASQ_OPE_OPC_ECI=new LygSlaveFilterRun();
-		SQ_OSU_MSQ_OSU_AVQ_ASQ_AOI_AOD_AOU_AOQ_VES=new LygSlaveFilterPanel((LygSlaveFilterRun) SQ_OSU_MSQ_OSU_AVQ_ASQ_OPE_OPC_ECI);
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_AOI_AOD_AOU_AOQ_VES
+		=new LygSlaveFilterPanel((LygSlaveFilterRun) SQ_OSU_MSQ_OSU_AVQ_ASQ_OPE_OPC_ECI);
 		SQ_OSU_MSQ_OSU_AVQ_ASQ_AOI_AOD_AOU_AOQ_VES.config();
 		showed=false;
 	}
-	public void OPE_E(JTextPane jTextPane)  throws FileNotFoundException, IOException, UnsupportedAudioFileException
-	{
-		((LygSlaveFilterRun) SQ_OSU_MSQ_OSU_AVQ_ASQ_OPE_OPC_ECI).run((LygSlaveFilterView) SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ);
+	public void OPE_E(JTextPane jTextPane)  throws FileNotFoundException, IOException, UnsupportedAudioFileException{
+		((LygSlaveFilterRun) SQ_OSU_MSQ_OSU_AVQ_ASQ_OPE_OPC_ECI)
+		.run((LygSlaveFilterView) SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ);
 	}
-	public void AVQ_ASQ_OVQ_OSQ_VSQ(JTextPane jTextPane)  throws Exception
-	{
+	public void AVQ_ASQ_OVQ_OSQ_VSQ(JTextPane jTextPane)  throws Exception{
 		SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ.view();
 		showed=true;
 	}
-	public OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI AOP_MVE_CSI_DUQ() throws CloneNotSupportedException, IOException 
-	{  
+	public OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI AOP_MVE_CSI_DUQ() throws CloneNotSupportedException, IOException {  
 	        SQ_OSU_MSQ_OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI = new LygSlaveFilterInterface();   
 	        return SQ_OSU_MSQ_OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI;  
 	}
