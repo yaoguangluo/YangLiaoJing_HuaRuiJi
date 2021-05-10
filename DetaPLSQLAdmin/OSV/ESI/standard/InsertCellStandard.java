a8e5dfc6aa997102d56ca1b216fc721a151cc65c
Deta Open Source <2080315360@qq.com>
2021-05-10 11:05:14 +0800

    1 增加了 plsql orm 插入行排序. 2 我稍后测试下. 3 我稍后会思考: 符号怎么进行简单的encode化, 省的要变通配符@tin@代替
    罗瑶光

---
+++ DetaPLSQLAdmin/OSV/ESI/standard/InsertCellStandard.java
@@ -0,0 +1,76 @@
+package OSV.ESI.standard;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import OSI.AOP.MEC.SIQ.plorm.PLORMImpl;
+import OSI.AOP.MEC.SIQ.plorm.PLORMInterf;
+import OSI.OPE.SI.SD.SU.SQ.ASU.OSU.PSU.MSU.AVQ.ASQ.ASU.MPE.procedure.pde.FullDNATokenPDI;
+//OSV PLSQL 研发, 思想, 设计: 罗瑶光 20210510
+//插入一行完整的数据.
+//我没有测试, 现在养疗经还用不到, 优先级稍后.
+//稍后会考虑不完整的插入情况 赋值empty 或null.
+public class InsertCellStandard{	
+	public List<String> txtToList() throws IOException{
+		List<String> dic_list= new ArrayList<>();
+		return dic_list;
+	}
+
+	public static Map<String, Object> InsertCellORM(String rootPath, String baseName, String tabKey
+			, Map<String, String> rowCells, Boolean initonEncrypt) throws IOException{
+		Iterator<String> iterators= rowCells.keySet().iterator();
+		Map<String, Object> map= null;
+		try {
+			PLORMInterf orm= new PLORMImpl();
+			orm= orm.startAtRootDir(rootPath)
+					.withBaseName(baseName)
+					.withTableInsert(tabKey);
+			while(iterators.hasNext()) {
+				String string= iterators.next();
+				if(initonEncrypt) {
+					orm= orm.withCulumnValue(string
+							, new FullDNATokenPDI().initonSect(rowCells.get(string).replace(":", "@Tin@")));
+				}else {
+					orm= orm.withCulumnValue(string, rowCells.get(string).replace(":", "@Tin@"));	
+					//我稍后会思考: 符号怎么进行实体化,省的要变通配符代替.
+				}	
+			}
+			orm.checkAndFixPlsqlGrammarErrors()//准备完善plsql orm语言 的语法检查函数 和修复函数。
+			.checkAndFixSystemEnvironmentErrors()//准备完善plsql orm语言 的系统环境检查函数和修复函数。
+			.finalExec(true);
+			//map= org.plsql.db.plsql.imp.ExecPLSQLImp.ExecPLORM(orm, true);
+		}catch(Exception e1) {
+			//准备写回滚
+			e1.printStackTrace();
+		}
+		return map;
+	}	
+
+	public static Map<String, Object> InsertCellPLSQL(String rootPath, String baseName, String tabKey
+			, Map<String, String> rowCells, Boolean initonEncrypt) throws IOException{
+		Iterator<String> iterators= rowCells.keySet().iterator();
+		Map<String, Object> map= null;
+		try {
+			String plsql= "setRoot:"+ rootPath+ ";";
+			plsql+= "baseName:"+ baseName+ ";"; 
+			plsql+= "tableName:"+ tabKey+ ":insert;";
+			while(iterators.hasNext()) {
+				String string= iterators.next();
+				if(initonEncrypt) {
+					plsql+= "culumnValue:"+ string+ ":"
+							+ new FullDNATokenPDI().initonSect(rowCells.get(string).replace(":", "@Tin@"))+ ";";
+				}else {
+					plsql+= "culumnValue:"+ string+ ":"+ rowCells.get(string).replace(":", "@Tin@")+ ";";
+				}
+			}	
+			map= OSI.OPE.ME.SM.OP.SM.AOP.MEC.SIQ.imp.ExecPLSQLImp.ExecPLSQL(plsql, true);
+		}catch(Exception e1) {
+			//准备写回滚
+			e1.printStackTrace();
+		}
+		return map;
+	}	
+}
