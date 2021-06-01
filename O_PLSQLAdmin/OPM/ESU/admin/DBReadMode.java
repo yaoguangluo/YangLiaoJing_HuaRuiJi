3942c5fa39064ab23fe7add5a259b9cbc4077b97
Deta Open Source <2080315360@qq.com>
2021-06-01 15:05:05 +0800

    1 设计了一个对象合并集合用上了, 准备测试下.
    罗瑶光

--- O_PLSQLAdmin/OPM/ESU/admin/DBReadMode.java
+++ O_PLSQLAdmin/OPM/ESU/admin/DBReadMode.java
@@ -54,17 +54,17 @@ public class DBReadMode{
 			app.dic_map= d.listToMap(dic_list);
 			app.dic_yw= app.dic_map;
 			app.dic_index= new DictionaryFromDB().mapToIndex(app.dic_map);
-			ObjectCombination.MapCombination(app.dic_li, d.mapToMap_li(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_hai, d.mapToMap_hai(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_xz, d.mapToMap_xz(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_ya, d.mapToMap_ya(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_jm, d.mapToMap_jm(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_xw, d.mapToMap_xw(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_cy, d.mapToMap_cy(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_jj, d.mapToMap_jj(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_zf, d.mapToMap_zf(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_cj, d.mapToMap_cj(app.dic_map));
-			ObjectCombination.MapCombination(app.dic_yl, d.mapToMap_yl(app.dic_map, app.dic_xw, app.dic_li, app.dic_xz, app.dic_jm));
+			ObjectCombination.mapCombination(app.dic_li, d.mapToMap_li(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_hai, d.mapToMap_hai(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_xz, d.mapToMap_xz(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_ya, d.mapToMap_ya(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_jm, d.mapToMap_jm(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_xw, d.mapToMap_xw(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_cy, d.mapToMap_cy(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_jj, d.mapToMap_jj(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_zf, d.mapToMap_zf(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_cj, d.mapToMap_cj(app.dic_map));
+			ObjectCombination.mapCombination(app.dic_yl, d.mapToMap_yl(app.dic_map, app.dic_xw, app.dic_li, app.dic_xz, app.dic_jm));
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -79,20 +79,20 @@ public class DBReadMode{
 		String tabKey= "zybc";
 		//以后设计 uniq forenkey等.
 		Map<String, Map<String, Object>> map= d.dbToMap(primaryKey, tabKey);
-		ObjectCombination.MapCombination(app.dic_index, map.get("中药名称"));
-		ObjectCombination.MapCombination(app.dic_yw, map.get("笔记原文"));
-		ObjectCombination.MapCombination(app.dic_li, map.get("功效"));
-		ObjectCombination.MapCombination(app.dic_xz, map.get("中医馆药理"));
-		ObjectCombination.MapCombination(app.dic_ya, map.get("愚按"));
-		ObjectCombination.MapCombination(app.dic_jm, map.get("经脉"));
-		ObjectCombination.MapCombination(app.dic_xw, map.get("性味"));
-		ObjectCombination.MapCombination(app.dic_cy, map.get("崇源"));
-		ObjectCombination.MapCombination(app.dic_jj, map.get("经解"));
-		ObjectCombination.MapCombination(app.dic_zf, map.get("搭配"));
-		ObjectCombination.MapCombination(app.dic_hai, map.get("风险规避"));
-		ObjectCombination.MapCombination(app.dic_cj, map.get("常见药"));
-		ObjectCombination.MapCombination(app.dic_yl, map.get("用量"));
-		ObjectCombination.MapCombination(app.dic_map, map.get("dic_map"));
+		ObjectCombination.mapCombination(app.dic_index, map.get("中药名称"));
+		ObjectCombination.mapCombination(app.dic_yw, map.get("笔记原文"));
+		ObjectCombination.mapCombination(app.dic_li, map.get("功效"));
+		ObjectCombination.mapCombination(app.dic_xz, map.get("中医馆药理"));
+		ObjectCombination.mapCombination(app.dic_ya, map.get("愚按"));
+		ObjectCombination.mapCombination(app.dic_jm, map.get("经脉"));
+		ObjectCombination.mapCombination(app.dic_xw, map.get("性味"));
+		ObjectCombination.mapCombination(app.dic_cy, map.get("崇源"));
+		ObjectCombination.mapCombination(app.dic_jj, map.get("经解"));
+		ObjectCombination.mapCombination(app.dic_zf, map.get("搭配"));
+		ObjectCombination.mapCombination(app.dic_hai, map.get("风险规避"));
+		ObjectCombination.mapCombination(app.dic_cj, map.get("常见药"));
+		ObjectCombination.mapCombination(app.dic_yl, map.get("用量"));
+		ObjectCombination.mapCombination(app.dic_map, map.get("dic_map"));
 	}
 
 	public static void readDBInWebWay(App app) {
--- P_Processor/PEU/P/combination/ObjectCombination.java
+++ P_Processor/PEU/P/combination/ObjectCombination.java
@@ -5,10 +5,10 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
+import java.util.Vector;
+//罗瑶光
 public class ObjectCombination {
-	//设计个合并函数 罗瑶光
-	public static Map<String, Object> MapCombination(Map<String, Object> output, Map<String, Object> destination) {
+	public static Map<String, Object> mapCombination(Map<String, Object> output, Map<String, Object> destination) {
 		if(null== output) {
 			output= new HashMap<>();
 		}
@@ -22,10 +22,97 @@ public class ObjectCombination {
 		return output;
 	}
 
-	//设计个合并函数 罗瑶光
-	public static List<Object> ListCombination(List<Object> output, List<Object> destination) {
+	public static List<Object> listCombination(List<Object> output, List<Object> destination) {
 		if(null== output) {
 			output= new LinkedList<>();
+		}
+		Iterator<Object> keyIterator= destination.iterator();
+		while(keyIterator.hasNext()) {
+			output.add(keyIterator.next());
+		}
+		return output;
+	}
+
+	public static int[] intArrayCombination(int[] output, int[] destination) {
+		int[] ints= new int[output.length+ destination.length];
+		for(int i= 0; i< output.length; i++) {
+			ints[i]= output[i];
+		} 
+		for(int i= 0; i< destination.length; i++) {
+			ints[i]= output[output.length+ i];
+		} 
+		return ints;
+	}
+	
+	public static double[] doubleArrayCombination(double[] output, double[] destination) {
+		double[] doubles= new double[output.length+ destination.length];
+		for(int i= 0; i< output.length; i++) {
+			doubles[i]= output[i];
+		} 
+		for(int i= 0; i< destination.length; i++) {
+			doubles[i]= output[output.length+ i];
+		} 
+		return doubles;
+	}
+	
+	public static long[] longArrayCombination(long[] output, long[] destination) {
+		long[] longs= new long[output.length+ destination.length];
+		for(int i= 0; i< output.length; i++) {
+			longs[i]= output[i];
+		} 
+		for(int i= 0; i< destination.length; i++) {
+			longs[i]= output[output.length+ i];
+		} 
+		return longs;
+	}
+	
+	public static float[] floatArrayCombination(float[] output, float[] destination) {
+		float[] floats= new float[output.length+ destination.length];
+		for(int i= 0; i< output.length; i++) {
+			floats[i]= output[i];
+		} 
+		for(int i= 0; i< destination.length; i++) {
+			floats[i]= output[output.length+ i];
+		} 
+		return floats;
+	}
+	
+	public static String[] StringArrayCombination(String[] output, String[] destination) {
+		String[] strings= new String[output.length+ destination.length];
+		for(int i= 0; i< output.length; i++) {
+			strings[i]= output[i];
+		} 
+		for(int i= 0; i< destination.length; i++) {
+			strings[i]= output[output.length+ i];
+		} 
+		return strings;
+	}
+	
+	public static char[] charArrayCombination(char[] output, char[] destination) {
+		char[] chars= new char[output.length+ destination.length];
+		for(int i= 0; i< output.length; i++) {
+			chars[i]= output[i];
+		} 
+		for(int i= 0; i< destination.length; i++) {
+			chars[i]= output[output.length+ i];
+		} 
+		return chars;
+	}
+	
+	public static byte[] byteArrayCombination(byte[] output, byte[] destination) {
+		byte[] bytes= new byte[output.length+ destination.length];
+		for(int i= 0; i< output.length; i++) {
+			bytes[i]= output[i];
+		} 
+		for(int i= 0; i< destination.length; i++) {
+			bytes[i]= output[output.length+ i];
+		} 
+		return bytes;
+	}
+	
+	public static Vector<Object> vectorCombination(Vector<Object> output, Vector<Object> destination) {
+		if(null== output) {
+			output= new Vector<>();
 		}
 		Iterator<Object> keyIterator= destination.iterator();
 		while(keyIterator.hasNext()) {
