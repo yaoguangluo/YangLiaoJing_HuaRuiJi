b480f33b3a72d997ae52e7ec0fcb063b23f8418c
Deta Open Source <2080315360@qq.com>
2021-06-02 13:34:02 +0800

    1 增加1个map分类原型验证机.
    罗瑶光

--- P_Processor/PEU/P/classification/ObjectClassification.java
+++ P_Processor/PEU/P/classification/ObjectClassification.java
@@ -27,6 +27,28 @@ public class ObjectClassification {
 		return output;
 	}
 	
+	@SuppressWarnings("unchecked")
+	public static Map<String, Map<String, Object>> mapClassification2D(Map<String, Object> input, String[] keywords) {
+		Map<String, Map<String, Object>> output= new HashMap<>();
+		Map<String, Object>[] outputArray= new HashMap[keywords.length];
+		for(int i= 0; i< keywords.length; i++) {
+			outputArray[i]= new HashMap<>();
+		}
+		Iterator<String> iterator= input.keySet().iterator();
+		while(iterator.hasNext()) {
+			String string= iterator.next();
+			for(int i= 0; i< keywords.length; i++) {
+				if(string.contains(keywords[i])) {
+					outputArray[i].put(string, input.get(string));
+				}
+			}
+		}
+		for(int i= 0; i< keywords.length; i++) {
+			output.put(keywords[i], outputArray[i]);
+		}
+		return output;
+	}
+	
 	public static void main(String[] ARGS) {
 		Map<String, Object> input= new HashMap<String, Object>();
 		String[] keywords= new String[3];
@@ -41,5 +63,6 @@ public class ObjectClassification {
 		keywords[1]="ัþ";
 		keywords[2]="นโ";
 		mapClassification(input, keywords);
+		mapClassification2D(input, keywords);
 	}
 }
